e08faa3a6aa9fe9b1226172f820b4f0cf1a38a56
Oliver Elliott <oliver.elliott@gmail.com>
2019-01-27 22:09:52 +0000

    fixed topology.dot

--- graph.sh
+++ graph.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp topology.dot graph.dot
 sed -i 's/:"swp/:"/g' graph.dot
+sed -i 's/graph vx {/graph vx {\n  graph [fontsize=8 fontname=Verdana compound=true];\n  node [shape=box style=filled]\n\n\n    { rank=same leaf01 leaf02 }\n      { rank=same spine01 spine02 }\n        { rank=same exit01 exit02 }\n	  { rank=max "oob-mgmt-server" "oob-mgmt-switch" }\n/g' graph.dot
 dot -Tpng -ograph.png graph.dot
 rm graph.dot
